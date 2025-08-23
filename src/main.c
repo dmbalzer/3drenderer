140be5bdfb11560e5bf84bbfa63c1391b40f1240
David Balzer <dmbalzer@gmail.com>
2025-08-22 21:03:51 -0400

    added main

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <SDL3/SDL.h>
+
+void init_window(void) {
+	if ( !SDL_Init(SDL_INIT_VIDEO) ) {
+		SDL_Log("%s", SDL_GetError());
+		exit(1);
+	}
+}
+
+int main(void) {
+	
+	if ( ! )
+
+	return 0;
+}
