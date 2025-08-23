8d6ce673cfbae95627c3ee4776c7179bb671620a
David Balzer <dmbalzer@gmail.com>
2025-08-23 08:46:35 -0400

    refactored buffer

---
+++ src/buffer.c
@@ -0,0 +1,43 @@
+#include "buffer.h"
+
+static int buffer_w = 0;
+static int buffer_h = 0;
+static Uint32* buffer = NULL;
+
+void buffer_init(int w, int h) {
+	buffer = (Uint32*)SDL_malloc(sizeof(Uint32) * w * h);
+	buffer_w = w;
+	buffer_h = h;
+}
+
+void buffer_free(void) {
+	SDL_free(buffer);
+}
+
+Uint32* buffer_get(void) {
+    return buffer;
+}
+
+void buffer_clear(Uint32 c) {
+	for ( int i = 0; i < buffer_w * buffer_h; i++ ) {
+		buffer[i] = c;
+	}
+}
+
+void buffer_draw_grid(void) {
+	for ( int x = 0; x < buffer_w; x += 10 ) {
+		for ( int y = 0; y < buffer_h; y += 10 ) {
+			buffer[x + y * buffer_w] = 0xFF505050;
+		}
+	}
+}
+
+void buffer_draw_rect(int x, int y, int w, int h, Uint32 c) {
+	for ( int _x = x; _x < x + w; _x++ ) {
+		for ( int _y = y; _y < y + h; _y++ ) {
+			if ( _x < 0 || _x >= buffer_w ) continue;
+			if ( _y < 0 || _y >= buffer_h ) continue;
+			buffer[_x + _y * buffer_w] = c;
+		}
+	}
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <SDL3/SDL.h>
 #include "sdl.h"
+#include "buffer.h"
 
 const int window_w = 960;
 const int window_h = 640;
@@ -7,44 +7,10 @@ int quit = 0;
 
 const bool* keys = NULL;
 
-Uint32* buffer = NULL;
-
-void buffer_init(void) {
-	buffer = (Uint32*)SDL_malloc(sizeof(Uint32) * window_w * window_h);
-}
-
-void buffer_free(void) {
-	SDL_free(buffer);
-}
-
-void buffer_clear(Uint32 c) {
-	for ( int i = 0; i < window_w * window_h; i++ ) {
-		buffer[i] = c;
-	}
-}
-
-void buffer_draw_grid(void) {
-	for ( int x = 0; x < window_w; x += 10 ) {
-		for ( int y = 0; y < window_h; y += 10 ) {
-			buffer[x + y * window_w] = 0xFF505050;
-		}
-	}
-}
-
-void buffer_draw_rect(int x, int y, int w, int h, Uint32 c) {
-	for ( int _x = x; _x < x + w; _x++ ) {
-		for ( int _y = y; _y < y + h; _y++ ) {
-			if ( _x < 0 || _x >= window_w ) continue;
-			if ( _y < 0 || _y >= window_h ) continue;
-			buffer[_x + _y * window_w] = c;
-		}
-	}
-}
-
 void setup(void) {
 	quit = sdl_init(window_w, window_h);
 	keys = SDL_GetKeyboardState(NULL);
-	buffer_init();
+	buffer_init(window_w, window_h);
 }
 
 void check_input(void) {
@@ -60,7 +26,7 @@ void draw(void) {
 	buffer_clear(0xFF000000);
 	buffer_draw_grid();
 	buffer_draw_rect(20,20,80,80,0xFFFFFF00);
-	sdl_render(buffer);
+	sdl_render(buffer_get());
 }
 
 void cleanup(void) {
