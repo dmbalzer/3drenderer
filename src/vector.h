2c22663da15ad4d9755b96e144d0b2500f94630a
David Balzer <dmbalzer@gmail.com>
2025-09-02 16:48:07 -0400

    added vector.h

---
+++ src/vector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_INCLUDED
+#define VECTOR_INCLUDED
+
+typedef struct {
+	float x;
+	float y;
+} Vector2_t;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+} Vector3_t;
+
+#endif // VECTOR_INCLUDED
